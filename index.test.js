73bcc88530eca66b4a0bc2d42a97fb0e1a3dbb62
Pascal Duez <public@pascalduez.me>
2024-09-27 10:13:02 +0200

    Mixed updates

--- index.test.js
+++ index.test.js
@@ -1,12 +1,20 @@
 // @flow
 
+// $FlowFixMe[cannot-resolve-module] Will need to be added to flow-typed node env.
 import { describe, it } from 'node:test';
 import assert from 'node:assert/strict';
 
-import { add } from './index.js';
+import { add, isString } from './index.js';
 
 describe('add', () => {
   it('should return the sum of params', () => {
     assert.equal(add(1, 2), 3);
+  });
+});
+
+describe('isString', () => {
+  it('should assert whether the type of param is a string', () => {
+    assert.equal(isString(''), true);
+    assert.equal(isString(123), false);
   });
 });
